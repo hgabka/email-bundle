dc6bfc68d71d3fa79354277648a903f162be5a66
Gabe <hgabka@dev.hgabka.eu>
2018-10-19 19:01:38 +0200

    fixes, templatevar method

--- src/Annotation/TemplateVar.php
+++ src/Annotation/TemplateVar.php
@@ -6,7 +6,7 @@ use Doctrine\Common\Annotations\Annotation;
 
 /**
  * @Annotation
- * @Target("PROPERTY")
+ * @Target({"PROPERTY","METHOD"})
  */
 class TemplateVar
 {
@@ -15,6 +15,9 @@ class TemplateVar
 
     /** @var string */
     public $placeholder;
+
+    /** @var string */
+    public $type;
 
     /**
      * @return string
@@ -52,6 +55,26 @@ class TemplateVar
     public function setPlaceholder($placeholder)
     {
         $this->placeholder = $placeholder;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * @param string $type
+     *
+     * @return TemplateVar
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
 
         return $this;
     }
