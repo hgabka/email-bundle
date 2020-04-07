ba2c3b405bd0224379d4d261fbeba8c2074ad0c4
hgabka <hgabka@gmail.com>
2020-04-07 19:15:17 +0200

    Update MessageLogger.php

--- src/Logger/MessageLogger.php
+++ src/Logger/MessageLogger.php
@@ -3,15 +3,16 @@
 namespace Hgabka\EmailBundle\Logger;
 
 use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
 use Psr\Log\LoggerInterface;
 
 class MessageLogger
 {
     protected $logger;
 
-    public function __construct(LoggerInterface $logger, $path)
+    public function __construct($path)
     {
-        $this->logger = $logger;
+        $this->logger = new Logger('message_logger');
         $handler = new StreamHandler($path.'/'.date('Ymd').'.log');
         $handler->setFormatter(new MessageLogFormatter());
         $this->logger->setHandlers([$handler]);
