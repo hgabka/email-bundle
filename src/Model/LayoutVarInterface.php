ec990b3d5460cae2103b654fbb1a3d88238e56e6
hgabka <hgabka@gmail.com>
2021-03-25 09:58:41 +0100

    Update LayoutVarInterface.php

--- src/Model/LayoutVarInterface.php
+++ src/Model/LayoutVarInterface.php
@@ -8,7 +8,7 @@ interface LayoutVarInterface
 
     public function getLabel();
 
-    public function getValue($bodyHtml, $params, $locale);
+    public function getValue($bodyHtml, $mail, $params, $locale);
 
     public function setPriority($priority);
 
