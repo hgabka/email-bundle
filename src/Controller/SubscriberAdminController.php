c879cf3306596d5894ed5806d7ce9208055860ee
hgabka <hgabka@gmail.com>
2021-12-09 14:39:33 +0100

    Update SubscriberAdminController.php

--- src/Controller/SubscriberAdminController.php
+++ src/Controller/SubscriberAdminController.php
@@ -4,11 +4,14 @@ namespace Hgabka\EmailBundle\Controller;
 
 use Sonata\AdminBundle\Controller\CRUDController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class SubscriberAdminController extends CRUDController
 {
-    protected function preEdit(Request $request, $object)
+    protected function preEdit(Request $request, object $object): ?Response
     {
         $object->setLists($this->admin->getManager()->getListsForSubscriber($object));
+        
+        return parent::preEdit($request, $object);
     }
 }
