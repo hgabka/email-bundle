85e821c643709d453b3d8d8152c82ce3f87b9329
hgabka <hgabka@gmail.com>
2022-05-06 13:14:55 +0200

    Update Configuration.php

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -19,7 +19,7 @@ class Configuration implements ConfigurationInterface
     /**
      * {@inheritdoc}
      */
-    public function getConfigTreeBuilder()
+    public function getConfigTreeBuilder(): TreeBuilder
     {
         $treeBuilder = new TreeBuilder('hgabka_email');
         $rootNode = $treeBuilder->getRootNode();
